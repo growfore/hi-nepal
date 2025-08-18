d5cb72d687bc772a224fe1e259a7a6d5a832ab7b
kshetritej <tejghartikshetri@gmail.com>
2025-08-18 14:06:21 +0545

    add: redirects

--- new-hinepal/next.config.mjs
+++ new-hinepal/next.config.mjs
@@ -268,6 +268,282 @@ const nextConfig = {
         destination: "/langtang-gosaikunda-trek",
         permanent: true,  
         statusCode: 301
+      },
+      {
+        source: "/wildlife-safari/chitwan-national-park/chitwan-national-park-wildlife-safari",
+        destination: "/chitwan-national-park-tour",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/manaslu-region/tsum-valley-trek",
+        destination: "/tsum-valley-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/manaslu-region/manaslu-circuit-trek",
+        destination: "/manaslu-circuit-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/wildlife-safari/bardiya-national-park/bardiya-national-park-safari",
+        destination: "/bardiya-national-park-tour",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/mustang-region/upper-mustang-trek",
+        destination: "/",
+        permanent: false,
+        statusCode: 302
+      },
+      {
+        source: "/trekking/manaslu-region/manaslu-and-tsum-valley-circuit-trek",
+        destination: "/manaslu-tsum-valley-circuit-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/langtang-region/langtang-gosaikunda-trek",
+        destination: "/langtang-gosaikunda-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/langtang-region/langtang-valley-trek",
+        destination: "/langtang-valley-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/langtang-region/helambu-trek",
+        destination: "/helambu-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/everest-region/three-pass-trek",
+        destination: "/three-passes-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/everest-region/renjo-la-pass-trek",
+        destination: "/renjo-la-pass-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/everest-region/pikey-peak-trek",
+        destination: "/pikey-peak-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/everest-region/gokyo-ri-trek",
+        destination: "/gokyo-valley-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/everest-region/everest-base-camp-trek",
+        destination: "/everest-base-camp-trek",
+        permanent: true,
+        statusCode: 301,
+      },
+      {
+        source: "/trekking/everest-region/chola-pass-gokyo-trek",
+        destination: "/chola-pass-gokyo-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/dolpa-region/upper-dolpa-trek",
+        destination: "/upper-dolpo-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/panchase-hill-trekking",
+        destination: "/panchase-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source:"/trekking/annapurna-region/nar-phu-valley-with-annapurna-circuit-trek",
+        destination: "/",
+        permanent: false,
+        statusCode: 302
+      },
+      {
+        source: "/trekking/dolpa-region/lower-dolpa-trek",
+        destination: "/lower-dolpo-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/north-annapurna-base-camp-trek",
+        destination: "/north-annapurna-base-camp-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/nar-phu-trek",
+        destination: "/",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/millennium-trek-nepal",
+        destination: "/",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/mardi-himal-treks",
+        destination: "/mardi-himal-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/kori-kopuche-treks",
+        destination: "/kapuche-lake-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/khopra-dhanda-hidden-lake-trek",
+        destination: "/khopra-danda-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/jomsom-muktinath-trek",
+        destination: "/jomsom-muktinath-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/australian-camp-trek",
+        destination: "/",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/around-annapurna-trekking",
+        destination: "/annapurna-base-camp-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/annapurna-panchase-hill-trekking",
+        destination: "/panchase-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/ghorepani-poon-hill-trek",
+        destination: "/ghorepani-poon-hill-trek",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/trekking/annapurna-region/annapurna-base-camp-trek",
+        destination: "/annapurna-base-camp-trek",
+        permanent: true,
+        statusCode: 301 
+      },
+      {
+        source: "/tour/multi-day-tour/nepal-tour-package",
+        destination: "/activities/tours",
+        permanent: true,
+        statusCode: 301 
+      },
+      {
+        source: "/tour/multi-day-tour/bardia-national-park",
+        destination: "/bardiya-national-park-tour",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/tour/day-hike/the-epic-kristi-village-hike",
+        destination: "/",
+        permanent: true,
+        statusCode: 301,
+      },
+      {
+        source: "/tour/multi-day-tour/jungle-safari-at-chitwan-national-park",
+        destination: "/chitwan-national-park-tour",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/tour/day-hike/day-hike-in-pokhara",
+        destination: "/pokhara-valley-tour",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+       source: "/tour/day-hike/australian-camp-day-hike",
+       destination: "/",
+       permanent: true,
+       statusCode: 301
+      },
+      {
+        source: "/heli-tour/annapurna-heli-tour/kapuche-helicopter-tour",
+        destination: "/kapuche-lake-trek",
+        permanent: true,
+        statusCode: 301
+      }, 
+      {
+        source: "/heli-tour/annapurna-heli-tour/experience-the-beautiful-of-kapuche-lake-by-helicopter",
+        destination: "/",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/heli-tour/annapurna-heli-tour/helicopter-rescue-services-in-nepal",
+        destination: "/",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/heli-tour/annapurna-heli-tour/abc-helicopter-tour",
+        destination: "/",
+        permanent: true,
+        statusCode: 301 
+      },
+      {
+        source: "/adventure-activity/rafting/upper-seti-rafting",
+        destination: "/adventure",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/adventure-activity/rafting/expreience-the-thrill-of-kali-gandaki-river-rafting",
+        destination: "/adventure",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/adventure-activity/hot-air-balloon/hot-air-balloon-in-pokhara",
+        destination: "/adventure",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/adventure-activity/rafting/trishuli-river-rafting",
+        destination: "/adventure",
+        permanent: true,
+        statusCode: 301
+      },
+      {
+        source: "/adventure-activity/atv-ride/atv-ride-in-pokhara",
+        destination: "/adventure",
+        permanent: true,
+        statusCode: 301
       }
     ];
   },

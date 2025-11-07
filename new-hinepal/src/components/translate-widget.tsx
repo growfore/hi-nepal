6ccc9ed84d7a9a87c96b1efed1aacd9ea2f12f2d
kshetritej <tejghartikshetri@gmail.com>
2025-11-07 12:29:15 +0545

    again add translate widget

--- new-hinepal/src/components/translate-widget.tsx
+++ new-hinepal/src/components/translate-widget.tsx
@@ -1,14 +1,14 @@
-// "use client";
+"use client";
 
-// import GoogleTranslate from "next-google-translate-widget";
+import GoogleTranslate from "next-google-translate-widget";
 
-// export default function TranslateWidget() {
-//   return (
-//     <div className="fixed bottom-4 left-4 z-[999]">
-//       <GoogleTranslate
-//         pageLanguage="en"
-//         includedLanguages="es,en,fr,zh-Hans,nl,ne,ru,uk,za,fi"
-//       />
-//     </div>
-//   );
-// }
+export default function TranslateWidget() {
+  return (
+    <div className="fixed bottom-4 left-4 z-999">
+      <GoogleTranslate
+        pageLanguage="en"
+        includedLanguages="es,en,fr,zh-Hans,nl,ne,ru,uk,za,fi"
+      />
+    </div>
+  );
+}

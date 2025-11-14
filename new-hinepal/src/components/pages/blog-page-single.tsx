c903a516b5ad18e43cd3348f4e2e02d154374431
kshetritej <tejghartikshetri@gmail.com>
2025-11-14 10:20:32 +0545

    move the image little bit low for the blog page

--- new-hinepal/src/components/pages/blog-page-single.tsx
+++ new-hinepal/src/components/pages/blog-page-single.tsx
@@ -2,10 +2,11 @@ import { formatRevalidate } from "@/helper/formate";
 import Cta from "@/components/organisms/cta";
 import { TBlog } from "@/types/types";
 import Image from "next/image";
+import { LucideUser } from "lucide-react";
 
 export function BlogPage({ blog }: { blog: TBlog }) {
   return (
-    <section className="pt-32 pb-12 ">
+    <section className="pt-46 pb-12 ">
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           <div className="">
@@ -39,18 +40,8 @@ export function BlogPage({ blog }: { blog: TBlog }) {
                 ></h1>
                 <div className="flex items-center gap-4 text-sm text-gray-600">
                   <div className="flex items-center gap-2">
-                    <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center">
-                      <svg
-                        className="w-4 h-4 text-white"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+                    <div className="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center p-2">
+                      <LucideUser className="text-white" />
                     </div>
                     <span className="font-medium">Hi Nepal Team</span>
                   </div>
@@ -66,17 +57,17 @@ export function BlogPage({ blog }: { blog: TBlog }) {
                 dangerouslySetInnerHTML={{ __html: blog.content }}
                 className="
   prose prose-lg max-w-none
-  prose-headings:text-gray-900 prose-headings:font-[700]
+  prose-headings:text-gray-900 prose-headings:font-bold
 
   prose-h1:text-4xl prose-h1:mb-6 prose-h1:mt-8
-  prose-h2:text-3xl prose-h2:mt-6 prose-h2:mb-2 prose-h2:font-[700]
+  prose-h2:text-3xl prose-h2:mt-6 prose-h2:mb-2 prose-h2:font-bold
   prose-h3:text-xl prose-h3:mt-4 prose-h3:mb-1
 
   prose-p:text-gray-700 prose-p:leading-relaxed prose-p:mb-4 prose-p:mt-0
 
   prose-a:text-green-600 prose-a:no-underline hover:prose-a:text-green-700 hover:prose-a:underline
 
-  prose-strong:text-black prose-strong:font-[700]
+  prose-strong:text-black prose-strong:font-bold
 
   prose-ul:my-2 prose-ol:my-2
   prose-li:text-gray-700 prose-li:mb-1

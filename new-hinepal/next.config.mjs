e672ba2152f429397ff04680579c805ed95df7b2
kshetritej <tejghartikshetri@gmail.com>
2026-01-25 14:00:49 +0545

    add: cache control in nextconfig

--- new-hinepal/next.config.mjs
+++ new-hinepal/next.config.mjs
@@ -687,6 +687,19 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/_next/image(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

3712b342bb480a060a6f80c89461006c78838610
kshetritej <tejghartikshetri@gmail.com>
2026-03-13 12:20:51 +0545

    ...

--- new-hinepal/src/components/gallery-skeleton.tsx
+++ new-hinepal/src/components/gallery-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const GallerySkeleton = () => {
   return (
-    <div className="grid grid-cols-3 min-h-40 md:min-h-120 gap-4 container mx-auto">
+    <div className="grid grid-cols-3 h-auto md:min-h-120 gap-4 container mx-auto">
       <Skeleton className="col-span-2 h-120" />
       <div className="col-span-1 h-120 gap-4 hidden md:grid">
         <Skeleton className="h-56 w-full" />
